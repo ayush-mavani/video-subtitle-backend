cba174c60f7f436d0c63629a543f1dbdd50fe8c5
Ayush Mavani <ayushmavani6@gmail.com>
2024-01-11 19:22:09 +0530

    fixed issue

--- utiils/multer.js
+++ utiils/multer.js
@@ -1,10 +1,12 @@
 const { v4: uuidv4 } = require("uuid");
 const multer = require("multer");
+const path = require("path");
 // Set up Multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     console.log("file::: ", file, req);
-    cb(null, "./public/");
+    // cb(null, "./public/");
+    cb(null, path.join(appRoot, "./public/"));
   },
   filename: (req, file, cb) => {
     const videoId = uuidv4();
