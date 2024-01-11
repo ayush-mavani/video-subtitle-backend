4865a225149b95a3bb661181223e36b0ff77dbca
Ayush Mavani <ayushmavani6@gmail.com>
2024-01-11 20:34:54 +0530

    fixed issue

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("./config/dbConnection");
 app.use(cors());
 app.use(express.json());
 // app.use("./public", express.static(path.join(__dirname, "public")));
-global.appRoot = path.resolve(__dirname);
+// global.appRoot = path.resolve(__dirname);
 
 // API Health check
 app.all("/api/health-check", (req, res) =>
--- utiils/multer.js
+++ utiils/multer.js
@@ -4,9 +4,8 @@ const path = require("path");
 // Set up Multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log("file::: ", file, req);
     // cb(null, "./public/");
-    cb(null, path.join(appRoot, "./public/"));
+    cb(null, path.join(__dirname, "../public/uploads"));
   },
   filename: (req, file, cb) => {
     const videoId = uuidv4();
