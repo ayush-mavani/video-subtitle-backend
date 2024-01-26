02a8ca2a640876fd4dd5b418edc36ad99c9692cc
Ayush Mavani <ayushmavani6@gmail.com>
2024-01-26 12:09:00 +0530

    fixed issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const routes = require("./router");
 require("dotenv").config();
 const app = express();
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 3000;
 
 // for db connection
 require("./config/dbConnection");
